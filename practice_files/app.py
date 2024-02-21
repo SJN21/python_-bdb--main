a5b61cc5efe8d646c342b1199d66da186e4c438b
Jainam <smartjainam03@gmail.com>
2024-02-21 23:03:51 +0530

    first commit

---
+++ practice_files/app.py
@@ -0,0 +1,13 @@
+# print("Hello")
+
+# variables
+name= "Jainam"
+age=20
+
+# print(name,'is',age)
+
+# strings
+# print('hi\nhow are you?')
+# print(f'hi\nhow are you {name}?', name[0], name[0:2], name.upper(), name.index('i'))
+
+# functions
